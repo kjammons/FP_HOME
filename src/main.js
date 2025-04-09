a99c557986e9bf3a51a5b7de72aaff96fc4da004
kjammons <80271162+kjammons@users.noreply.github.com>
2025-04-08 21:49:25 -0400

    more pages fussing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-// main.js
+// src/main.js
 import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),
 });
 
 export default app;
+
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  // Uncomment and adjust the base URL if needed:
+  // base: '/FP_HOME/',
 });
