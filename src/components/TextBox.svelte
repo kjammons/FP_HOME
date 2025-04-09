f1ec704fc0c96d8656b21e66f21a79f873d7e49c
kjammons <80271162+kjammons@users.noreply.github.com>
2025-04-09 12:37:32 -0400

    added  text box and setting up repo to deploy to pages

---
+++ src/components/TextBox.svelte
@@ -0,0 +1,22 @@
+<!-- src/components/TextBox.svelte -->
+<script>
+    
+    export let text = "This map displays various municipalities along with their homeownership rates. Hover over a municipality for details.";
+  </script>
+  
+  <div class="text-box">
+    {text}
+  </div>
+  
+  <style>
+    .text-box {
+      padding: 10px;
+      background-color: #f8f8f8;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      font-size: 1rem;
+      color: #333;
+      margin-top: 20px;
+    }
+  </style>
+  
