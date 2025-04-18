4af14f1fd80cb7d75d82d9784a159fa794b50c56
Emilia Charno <ercharno@mit.edu>
2025-04-18 11:43:56 -0400

    add covenants images

--- src/covenants/+page.svelte
+++ src/covenants/+page.svelte
@@ -1,4 +1,24 @@
 <script>
+
+/* deeds = {
+    "1": {
+        "name": "covenant_1",
+        "description": "first covenant",
+        "type": "covenant",
+        "id": 1,
+        "image": "public/images/"TITLE HERE"
+    }
+}   
+
+{#each deed in deeds}
+
+/* build out script for first covenant,then repeat */
+
+/* <div id = "1">
+
+</div> */
+
+
 </script>
 
 <h1>YAHOO</h1>
