8954bdc03784268199414ab682b53f81a713896d
Marine Gapihan <marine22@mit.edu>
2025-04-24 18:19:52 -0400

    04/24 final

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import RaceBarChart from './RaceBarChart.svelte';
+    import HomeownBar from './HomeownBar.svelte';
     let selectedYear = 2020;
 
     const decades = [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020];
@@ -14,3 +15,4 @@
   </select>
 
   <RaceBarChart year={selectedYear} />
+  <HomeownBar year={selectedYear} />
