a48e3c62e9026fadf42a7f047916b4fba7900502
kjammons <80271162+kjammons@users.noreply.github.com>
2025-04-27 23:20:48 -0400

    debugging some pages things...

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      pages: 'dist',
-      assets: 'dist',
-      fallback: null
-    }),
-    paths: {
-      base: process.env.VITE_BASE || ''
-    }
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist',
+			fallback: null
+		})
+	}
 };
+
+export default config;
