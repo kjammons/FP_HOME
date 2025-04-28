28d8dcb897be9c572a3ae27ef395426e07d5b86d
kjammons <80271162+kjammons@users.noreply.github.com>
2025-04-27 23:01:02 -0400

    debugging some pages things...

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-	  include: ['pdfjs'],
-	},
-});
+export default {
+  preprocess: vitePreprocess(),
 
+  kit: {
+    adapter: adapter({
+      pages: 'dist',
+      assets: 'dist',
+      fallback: null
+    }),
+    paths: {
+      base: process.env.VITE_BASE || ''
+    }
+  }
+};
