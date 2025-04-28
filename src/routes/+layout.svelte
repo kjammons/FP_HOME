9bdde66a8a8412527687c6724a573ccd01743915
kjammons <80271162+kjammons@users.noreply.github.com>
2025-04-27 23:42:41 -0400

    still debugging pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,10 @@
         
     let pages = [
         { url: "/", title: "Home" },
-        { url: "/covenants", title: "Covenants" },
-        { url: "/timeline", title: "Timeline" },
-        { url: "/white_homeownership", title: "Homeownership Map" },
-        { url: "/reading_list", "title": "Reading List" },
+        { url: "covenants", title: "Covenants" },
+        { url: "timeline", title: "Timeline" },
+        { url: "white_homeownership", title: "Homeownership Map" },
+        { url: "reading_list", "title": "Reading List" },
         { url: "https://dusp.mit.edu/projects/massachusetts-covenants-project#:~:text=This%20research%20project%20is%20systematically,the%20United%20States%3A%20restrictive%20covenants.", "title": "Get Involved" },
 
     ];
